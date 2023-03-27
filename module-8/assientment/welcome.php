d2ffe0c4ef083854f463b3567edce5295744c506
nasirdiu <nasir.joypurhat@gmail.com>
2023-03-27 23:02:34 +0600

    aaaaa

--- module-8/assientment/welcome.php
+++ module-8/assientment/welcome.php
@@ -1,16 +1,21 @@
-
 <?php
-
-function showUser($user) {
-echo  <<<NED
-    <ul class="bg-white shadow-lg p-6 mb-8">
-        <li class="mb-2">Time: {$user[0]}</li>
-        <li class="mb-2"><strong class="font-semibold">Name:</strong> {$user[1]}</li>
-        <li class="mb-2"><strong class="font-semibold">Email:</strong> {$user[2]}</li>
-        <li class="mb-2"><strong class="font-semibold">Password:</strong> {$user[3]}</li>
-        <li class="mb-2"><strong class="font-semibold">Profile Picture:</strong> <img src={$user[4]} alt={$user[4]} class="mx-auto shadow-lg" style="width: 300px; margin-top:10px; height: 300px; object-fit: cover;"></li>
-    </ul>
-NED;
-}
-
+//include auth_session.php file on all user panel pages
+include("auth_session.php");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+</head>
+<body>
+    <div class="form">
+        <h1>Hey, <?php echo $_SESSION['fname']; ?>!</h1>
+        <h2>You are now user dashboard page.</h2>
+        <p><a href="logout.php">Logout</a></p>
+    </div>
+</body>
+</html>
