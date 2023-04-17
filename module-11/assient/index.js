a45e1973649e599608ebfba4ab121de6c091d93e
nasirdiu <nasir.joypurhat@gmail.com>
2023-04-17 22:56:59 +0600

    a

--- module-11/assient/index.js
+++ module-11/assient/index.js
@@ -1,6 +1,8 @@
-const number = 5; 
-
-for (let i = 1; i <= 10; i++) {
-  const result = i * number;
-  console.log(`${number} x ${i} = ${result}`);
+//Assignment 11 - Multiplication table
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    const product = num * i;
+    console.log(num + ' x ' + i + ' = ' + product);
+  }
 }
+multiplicationTable(5);
